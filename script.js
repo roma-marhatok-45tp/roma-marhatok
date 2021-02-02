d21cffd6322f1e478d20a1a336a12cd77d5f7c3a
roma <roma.marhatok@yandex.by>
2021-02-02 21:43:11 +0300

    first dz 1.2

---
+++ script.js
@@ -0,0 +1,18 @@
+var a=0
+function change(){
+        
+        a++;
+            if(a>2){
+                a=1
+            }
+            if(a==1){
+                document.body.style.background="#499288"
+                
+            }
+            else if(a==2){
+                document.body.style.background="black"
+            }
+    }
+    function nextpage(){
+        window.location.href="https://ru.wikipedia.org/wiki/Всемирный_конгресс_пауэрлифтинга";
+   }
