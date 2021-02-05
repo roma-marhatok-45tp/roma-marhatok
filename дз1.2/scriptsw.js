fadcc6aca9d74f35d1210298e42ca8da1923386d
roma-marhatok-45tp <78157630+roma-marhatok-45tp@users.noreply.github.com>
2021-02-05 21:21:38 +0300

    Add files via upload

---
+++ дз1.2/scriptsw.js
@@ -0,0 +1,12 @@
+var button=document.getElementById('buttonid')
+button.addEventListener( 'click' , function() {
+    
+    document.body.classList.toggle('pagedark')
+
+})
+var button1=document.getElementById('button1')
+button1.addEventListener( 'click', function() {
+    
+    window.location.href='https://jdevelop.info/articles/html-css-js/200-perekhod-na-druguyu-stranitsu-s-pomoshchyu-javascript'
+   
+})
